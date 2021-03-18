b73b14020cf35fc9792acb47dd81a4c0a4e594d5
wzc <2353276788@qq.com>
2021-03-18 20:10:17 +0800

    init repo

---
+++ src/test/java/dgut/wzc/community/CommunityApplicationTests.java
@@ -0,0 +1,13 @@
+package dgut.wzc.community;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommunityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
